34b44aca73669fdffcf8662ed82533041e5556b3
Marcos Gabriel Ribeiro Souto <marcosgrsouto@gmail.com>
2021-09-06 15:18:44 -0300

    First commit

---
+++ src/main/java/com/marcosgrs/ribeirosevents/RibeirosEventsApplication.java
@@ -0,0 +1,13 @@
+package com.marcosgrs.ribeirosevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibeirosEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibeirosEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosgrs/ribeirosevents/RibeirosEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosgrs.ribeirosevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibeirosEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
